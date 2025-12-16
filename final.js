06a947f6ad05370ad368035e2454dd763c96711a
KASSAM-KHAN <m.kassam.khan2007@gmail.com>
2025-12-16 20:51:21 +0500

    Add files via upload

---
+++ final.js
@@ -0,0 +1,11 @@
+function zoomImage(imageSrc) {
+    window.open(
+        imageSrc,
+        "_blank",
+        "width=800,height=600"
+    );
+}
+
+function zoomImage(src) {
+    window.open(src, "_blank", "width=600,height=600");
+}
